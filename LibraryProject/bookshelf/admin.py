a3d4922e16f4f3e8163282573b7d4dcceeabb1f3
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-02 23:38:01 +0300

    Delete LibraryProject directory

--- LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-admin.site.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_year')
-    search_fields = ('title', 'author')
-
-# Register your models here.
--- LibraryProject/bookshelf/create.md
+++
@@ -1,6 +0,0 @@
-# Create Book
-```python
-from bookshelf.models import Book
-newbook = Book(title='1984', author='George Orwell', publication_year=1949)
-newbook.save() 
-# Expected Output: <Book: 1984>
