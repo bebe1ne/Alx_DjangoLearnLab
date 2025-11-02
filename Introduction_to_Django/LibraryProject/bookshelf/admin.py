bb571768f82c19055776524cc5c7a12756b820e9
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-03 01:09:49 +0300

    Fix typo in list_filter for BookAdmin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,7 +5,8 @@ admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'published_year')
     search_fields = ('title', 'author')
-    list_filter = ('published_year')
+    list_filter = ('publication_year')
 
 # Register your models here.
+
 
