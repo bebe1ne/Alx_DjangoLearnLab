0ee8d7ca05402a3cb102ece31dfd88ca59790164
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-09 22:30:26 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
+    path('books/add/', views.add_book_view, name='add_book'),
+    path('books/edit/<int:pk>/', views.edit_book_view, name='edit_book'),
+    path('books/delete/<int:pk>/', views.delete_book_view, name='delete_book'),
 ]
+
