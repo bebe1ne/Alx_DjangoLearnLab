79b1f8c4a93af9214b424ce0961eeca764018e36
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-09 23:01:24 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/templates/relationshi_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/templates/relationshi_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
@@ -16,5 +16,6 @@ urlpatterns = [
     path('books/delete/<int:pk>/', views.delete_book_view, name='delete_book'),
     path('admin/', views.admin_view, name='admin_view'),
 ]
+
 
 
