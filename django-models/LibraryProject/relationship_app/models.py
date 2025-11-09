b73553b81af5323cff88323b58cb425bb87e2831
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-09 22:45:04 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 # Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -48,7 +47,7 @@ class UserProfile(models.Model):
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     )
-    
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
