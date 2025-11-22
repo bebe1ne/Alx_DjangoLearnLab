23de66751edf3b84d58c034ed7a1556ff6f29e8f
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-23 00:49:49 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -14,3 +14,12 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("username", "email", "date_of_birth", "profile_photo")
+
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100, required=True)
+    another_field = forms.IntegerField(required=False)
+
+    def clean_example_field(self):
+        data = self.cleaned_data['example_field']
+        # Perform some validation or transformation on example_field
+        return data
