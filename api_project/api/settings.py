c7312b123b3435286ca84469ed0ab49ee4937225
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-23 16:48:56 +0300

    Update settings.py

---
+++ api_project/api/settings.py
@@ -0,0 +1,25 @@
+# settings.py
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',  # Adding auth token for authentication
+    'api',  # Your Django app
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+# Other necessary Django settings...
