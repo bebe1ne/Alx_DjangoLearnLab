a9b87ed3b44d2d801536325ab660068e5128d950
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-30 22:27:08 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),  # Unusual path for updating
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),  # Unusual path for deleting
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-pk-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-pk-delete'),
 ]
