c0446ed9fb5cb188b237ce42683b51c4448711a3
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-30 18:49:15 +0300

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 class Author(models.Model):
@@ -8,3 +7,26 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+api/serializers.py:
+
+from rest_framework import serializers
+from .models import Author, Book
+import datetime
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+    
+    def validate_publication_year(self, value):
+        current_year = datetime.datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
