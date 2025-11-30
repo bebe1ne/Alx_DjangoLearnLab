7372e1ca3e187ce4c342a60529fed01c57c0b9ea
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-11-30 22:39:52 +0300

    Update test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,20 +1,54 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Book, Author
+from .models import Author, Book
 
-class BookTests(APITestCase):
+class BookAPITests(APITestCase):
+
     def setUp(self):
         self.author = Author.objects.create(name="Test Author")
-        self.book_url = reverse('book-list')
-
+        self.book = Book.objects.create(title="Test Book", publication_year=2020, author=self.author)
+    
     def test_create_book(self):
-        data = {"title": "New Book", "publication_year": 2023, "author": self.author.id}
-        response = self.client.post(self.book_url, data)
+        url = reverse('book-list')
+        data = {'title': 'New Book', 'publication_year': 2021, 'author': self.author.id}
+        response = self.client.post(url, data, format='json')
+        # Status code check
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Response data check
+        self.assertEqual(response.data['title'], 'New Book')
 
-    def test_retrieve_book(self):
-        book = Book.objects.create(title="Existing Book", publication_year=2020, author=self.author)
-        response = self.client.get(reverse('book-detail', args=[book.id]))
+    def test_get_books(self):
+        url = reverse('book-list')
+        response = self.client.get(url, format='json')
+        # Status code check
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-requirements.txt:
+        # Response data check
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], "Test Book")
+
+    def test_get_book_detail(self):
+        url = reverse('book-detail', kwargs={'pk': self.book.pk})
+        response = self.client.get(url, format='json')
+        # Status code check
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Response data check
+        self.assertEqual(response.data['title'], "Test Book")
+
+    def test_update_book(self):
+        url = reverse('book-detail', kwargs={'pk': self.book.pk})
+        data = {'title': 'Updated Book'}
+        response = self.client.patch(url, data, format='json')
+        # Status code check
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Response data check
+        self.assertEqual(response.data['title'], 'Updated Book')
+
+    def test_delete_book(self):
+        url = reverse('book-detail', kwargs={'pk': self.book.pk})
+        response = self.client.delete(url)
+        # Status code check
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        # Confirm the book no longer exists
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
