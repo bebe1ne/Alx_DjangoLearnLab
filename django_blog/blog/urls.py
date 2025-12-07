a3ecf4c9139701f8834cbb99eb77004184c02b91
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-12-07 21:46:10 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,8 @@
+
 from django.urls import path
 from .views import (
-   ListView,DetailView, PostView,
-    PostUpdateView, PostDeleteView, register,
-    profile, search, comment_delete
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
+    CommentCreateView, CommentUpdateView, CommentDeleteView, register, profile, search
 )
 from django.contrib.auth import views as auth_views
 
@@ -13,7 +13,9 @@ urlpatterns = [
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('comment/<int:pk>/delete/', comment_delete, name='comment_delete'),
+    path('post/<int:post_pk>/comment/new/', CommentCreateView.as_view(), name='comment_new'),
+    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logged_out.html'), name='logout'),
