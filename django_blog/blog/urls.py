ff967a855887875a1f6156ed7f3aa4e6c9f388cb
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-12-07 22:00:55 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
-    CommentCreateView, CommentUpdateView, CommentDeleteView, register, profile, search
+    CommentCreateView, CommentUpdateView, CommentDeleteView, register, profile, search, PostByTagListView
 )
 from django.contrib.auth import views as auth_views
 
@@ -12,10 +12,11 @@ urlpatterns = [
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_new'),  # For creating a new comment
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),  # For editing a comment
-    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),  # For updating a comment
-    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),  # For deleting a comment
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_new'),
+    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='post_by_tag'),  # For viewing posts by tag
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logged_out.html'), name='logout'),
