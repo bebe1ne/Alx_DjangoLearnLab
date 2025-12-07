bf46388c39425d723bd68187d0102b0c6b9b4ae8
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-12-07 21:49:08 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
-    PostListView, PostDetailView, PostCreateView, PostUpdateView PostDeleteView,
-   CreateView, CommentUpdateView, CommentDeleteView, register, profile, search
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
+    CommentCreateView, CommentUpdateView, CommentDeleteView, register, profile, search
 )
 from django.contrib.auth import views as auth_views
 
@@ -12,7 +12,7 @@ urlpatterns = [
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('post/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name='comment_new'),  # For creating a new comment
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_new'),  # For creating a new comment
     path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),  # For editing a comment
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),  # For updating a comment
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),  # For deleting a comment
