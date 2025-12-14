dfd9d69abcfbb1fcd7d96f7d142db0252f43b32d
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-12-14 22:35:59 +0300

    Update manage.py

--- task_manager/manage.py
+++ task_manager/manage.py
@@ -14,12 +14,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-# task_manager/__init__.py
-# task_manager/asgi.py
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_manager.settings")
-
-application = get_asgi_application()
